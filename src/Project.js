2bd308fb3b6afdd02033e198018ebfbd2cb75955
Htoo Wai Yan <95065183+h2waiyan@users.noreply.github.com>
2023-09-30 14:10:21 +0630

    fix mobile responsive

--- src/Project.js
+++ src/Project.js
@@ -1,19 +1,22 @@
 import React from "react";
-import rectangleImage from "./images/profile.jpg";
+import rectangleImage from "./images/heingp-transportation.png";
+import bagan from "./images/profile.jpg";
 
-const ProjectShowcase = ({ title, subtitle }) => {
+const ProjectShowcase = ({ title, subtitle, imgsrc }) => {
   return (
-    <div className="flex items-center">
+    <div className="flex items-start flex-col sm:flex-row my-3">
       <div className="flex flex-col justify-center mr-8 flex-1">
         <h2 className="text-xl sm:text-2xl md:text-4xl font-bold text-gray-900 mb-2">
           {title}
         </h2>
-        <p className="text-gray-700">{subtitle}</p>
+        <p className="text-md sm:text-lg md:text-xl text-gray-700 mb-2">
+          {subtitle}
+        </p>
       </div>
       <img
-        src={rectangleImage}
-        alt="Rectangle"
-        className="w-64 h-48 filter grayscale"
+        src={imgsrc}
+        alt="project-img"
+        className="w-full sm:w-64 h-48 object-cover filter grayscale sm:ml-8"
       />
     </div>
   );
@@ -30,18 +33,22 @@ const Projects = () => {
           <ProjectShowcase
             title="Class on Cloud"
             subtitle="An online communication platform for students, teachers and parents"
+            imgsrc={bagan}
           />
           <ProjectShowcase
             title="Hein Group Online Ticketing System"
             subtitle="An app where passengers can book their trip online"
+            imgsrc={rectangleImage}
           />
           <ProjectShowcase
             title="Sein Kyaw Moe Gold and Jwellery"
             subtitle="A Point of Sale System for a gold shop"
+            imgsrc={bagan}
           />
           <ProjectShowcase
             title="Hein Group Business"
-            subtitle="An cash book app for a transportaion company"
+            subtitle="A cash book app for a transportaion company"
+            imgsrc={rectangleImage}
           />
         </div>
       </div>
