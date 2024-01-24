e973760d6f4ab515cb47b5e9d548bc91905898d1
Htoo Wai Yan <htoowaiyan.dev@gmail.com>
2024-01-24 12:07:01 +0630

    added profile photo
    Added a profile photo in home page and edit the footer with email and phone

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -4,8 +4,10 @@ const Footer = () => {
   return (
     <div className="bg-gray-900 text-white py-4">
       <div className="container mx-auto px-4 flex justify-center">
+
         <p className="text-center">
-          Made with Love © {new Date().getFullYear()}
+          h2wy.mog@gmail.com  |  htoowaiyan.dev@gmail.com  |  +959 761 315 566
+          {/* Made with Love © {new Date().getFullYear()} */}
         </p>
       </div>
     </div>
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -5,6 +5,7 @@ import Projects from "../Projects/Project";
 import Tech from "../Tech/Tech";
 import Footer from "../Footer/Footer";
 import CV from "../htoowaiyan-cv.pdf";
+import pp from '../images/pp.png'
 
 function Home() {
   return (
@@ -16,7 +17,8 @@ function Home() {
         backgroundPosition: "center",
       }}
     >
-      <div className="mt-20">
+      <div className="mt-15 flex flex-col justify-center items-center ">
+        <img src={pp} className="filter grayscale hover:grayscale-0 rounded-full w-20 h-20 sm:w-48 sm:h-32 md:w-32 md:h-32 object-cover mb-3" />
         <h1 className="name text-4xl sm:text-6xl md:text-8xl  text-amber sm:font-bold mb-4 text-center transition-all duration-500">
           HTOO WAI YAN
         </h1>
